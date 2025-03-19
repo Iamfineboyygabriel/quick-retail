ec4126e7effea5ab902a9fd726c395e6bf637e4c
honourableola <olalekanyunus2013@gmail.com>
2025-03-19 17:09:10 +0100

    updated app and constants

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-red-500">WELCOME GUYS!👋👋🤚</h1>
-      <Button type="submit">WAIT</Button>
+      <Button type="submit" variant="default">WAIT</Button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Notification } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { theme } from "./constants/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
+      <Notification />
       <App />
     </MantineProvider>
   </StrictMode>
