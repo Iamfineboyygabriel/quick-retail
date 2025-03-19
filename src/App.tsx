faafe6275bc820e069222d03a63fb8b0709d4d59
iamfineboyygabriel <tundegabriel999@gmail.com>
2025-03-19 14:36:20 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "@mantine/core";
 export default function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-red-500">WELCOME GUYS!👋👋</h1>
+      <h1 className="text-red-500">WELCOME GUYS!👋👋🤚</h1>
       <Button type="submit">WAIT</Button>
     </div>
   );
