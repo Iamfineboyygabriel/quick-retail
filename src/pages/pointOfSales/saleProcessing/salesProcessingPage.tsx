1bc7b8fb78c89a201134361db9b7923e0763ed67
iamfineboyygabriel <tundegabriel999@gmail.com>
2025-03-24 00:11:45 +0100

    update branch

--- src/pages/pointOfSales/saleProcessing/salesProcessingPage.tsx
+++ src/pages/pointOfSales/saleProcessing/salesProcessingPage.tsx
@@ -1,6 +1,8 @@
 import { Text } from "@mantine/core";
 import PageContainer from "../../../layout/pageContainer";
 import CustomerOrdersTable from "../../../components/dashboard/pointOfSales/dashboard/orderTable";
+import { ROUTES } from "../../../constants/routes";
+import { Link } from "react-router";
 
 const SalesProcessingPage = () => {
   const subHeaders = [
@@ -9,6 +11,7 @@ const SalesProcessingPage = () => {
         <Text fw={500} size="xl" c="black">
           Sale Processing
         </Text>
+        <Link to={ROUTES.createOrder}>Create Order</Link>
       </div>
     </div>,
   ];
