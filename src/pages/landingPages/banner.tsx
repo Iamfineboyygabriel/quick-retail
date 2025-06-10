7e2dc74258c9b06b08afe97bc891a817ada5b086
iamfineboyygabriel <tundegabriel999@gmail.com>
2025-06-10 10:40:06 +0100

    fixes

---
+++ src/pages/landingPages/banner.tsx
@@ -0,0 +1,11 @@
+import frame from "../../assets/images/banner.png";
+
+const Banner = () => {
+  return (
+    <div className="w-full">
+      <img src={frame} alt="frame" className="w-full h-auto" />
+    </div>
+  );
+};
+
+export default Banner;
